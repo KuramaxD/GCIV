f21c2a7555cbffda8575fde3d290904a27bd1fa6
Kurama <sonexa@gmail.com>
2016-01-03 09:47:57 -0200

    Limpeza nos arquivos

--- Game Server/Data/GlobalDefs.pas
+++ Game Server/Data/GlobalDefs.pas
@@ -19,14 +19,9 @@ const
 type
 
   TCLPID = (
-    CLPID_HACKLIST_REQUEST                  = $0017,
-    CLPID_CHECKLIST_REQUEST                 = $001B,
     CLPID_PLAYER_LOGIN                      = $0002,
     CLPID_KEEPALIVE                         = $0000,
     CLPID_CHAT                              = $0006,
-
-    CLPID_UNKNOWN_11                        = $0011,
-    CLPID_UNKNOWN_19                        = $0019,
     CLPID_NOTHING                           = $FFFF,
 
 
@@ -43,19 +38,6 @@ type
     SVPID_UNKNOWN_0611                      = $0611,
     SVPID_UNKNOWN_0414                      = $0414,
 
-    SVPID_WRONGPASS                         = $0003,
-    SVPID_SERVERLIST                        = $0004,
-    SVPID_UNKNOWN_5                         = $0005,
-    SVPID_LOADING                           = $0018,
-    SVPID_REQUEST_CHECK                     = $001C,
-    SVPID_UNKNOWN_2                         = $001A,
-    SVPID_UNKNOWN_9                         = $0009,
-    SVPID_UNKNOWN_A                         = $000A,
-    SVPID_UNKNOWN_E                         = $000E,
-    SVPID_UNKNOWN_F                         = $000F,
-    SVPID_UNKNOWN_1F                        = $001F,
-    SVPID_UNKNOWN_1A                        = $001A,
-    SVPID_UNKNOWN_12                        = $0012,
     SVPID_NOTHING                           = $FFFF
   );
 
